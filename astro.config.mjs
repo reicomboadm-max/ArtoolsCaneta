4ed7a401b560ec5d4668cf30bdc7ca4ef786d6be
Rei Combo <reicomboadm@gmil.com>
2026-08-27 10:42:53 -0300

    feat(seo): configura geracao automatica de sitemap XML e robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://artools-caneta.vercel.app',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date(),
+    }),
+  ],
+});
